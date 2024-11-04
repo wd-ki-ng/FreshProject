b09950068930ddb28f47477df69f63866d852fd8
ki_ng <eowhd171@naver.com>
2024-11-04 12:53:24 +0900

    Project First setting

---
+++ src/main/java/org/fresh/controller/BoardController.java
@@ -0,0 +1,14 @@
+package org.fresh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
